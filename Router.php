3f140cca899e5720f47218b54ceb044a3effddc3
Marta B. <105872099+mbf87@users.noreply.github.com>
2025-01-20 16:20:39 +0100

    Add files via upload

--- Router.php
+++ Router.php
@@ -16,7 +16,8 @@ class Router
     }
 
     public function comprobarRutas() {
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
+       // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
